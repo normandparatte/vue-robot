0e39a8f2ac88f08521dc491b324c334d36e2c973
normandparatte <normand.paratte@gmail.com>
2017-07-03 16:45:15 +0200

    Ajout du routeur et passage du code dans les bon composants

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,53 +1,95 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+     <h1 v-if="on">
+        {{message}}
+      </h1>
+      <input type="text" v-model="nom" placeholder="Nom">
+      <input type="text" v-model="prenom" placeholder="Prénom">
+      <input type="color" v-model="couleur" placeholder="Couleur">
+      <input type="button" v-on:click="ajouterPersonneListe()" value="Ajouter">     
+      <input type="text" v-model="query" placeholder="Rechercher">  
+      <ul>
+        <li v-for="(personne, index) in listeFiltree" v-bind:key="index" v-bind:style="{color: personne.couleur}" v-bind:class="{ws: index===1}">
+          {{ index+1 }} {{ personne.nom }} {{ personne.prenom }}
+          <img v-bind:src="personne | enAvatar(index)">
+          <input type="button" v-on:click="retirerPersonneListe(personne)" value="Supprimer de la liste"> 
+        </li>
+      </ul>
   </div>
 </template>
 
 <script>
+const CLE_VALEURS_LISTE = 'ValeursListe';
+
 export default {
   name: 'hello',
   data () {
+    let maList;
+    try {
+      maList = JSON.parse(localStorage.getItem(CLE_VALEURS_LISTE));
+
+      // Protection si null
+      if (!maList) {
+        throw new Error();
+      }
+    } catch (e) {
+      maList = [{
+        nom: 'Normand',
+        prenom: 'Normand',
+        couleur: '00FFFF'
+      }];
+    }
+
     return {
-      msg: 'Welcome to Your Vue.js App'
+      on: true,
+      message: 'Quel robot es-tu ?',
+      message2: '',
+      nom: '',
+      prenom: '',
+      couleur: '',
+      query: '',
+      maList
     };
+  },
+  methods: {
+    ajouterPersonneListe () {
+      this.maList.push({nom: this.nom, prenom: this.prenom, couleur: this.couleur});
+    },
+    retirerPersonneListe (personne) {
+      this.maList.splice(this.maList.indexOf(personne), 1);
+    }
+  },
+  filters: {
+    enAvatar (personne, index) {
+      index++;
+      return 'https://robohash.org/' + personne.prenom + personne.nom + '/';
+    }
+  },
+  computed: {
+    listeFiltree () {
+      return this.maList.filter(personne => {
+        return personne.nom.toLowerCase().indexOf(this.query.toLowerCase()) > -1 || personne.prenom.toLowerCase().indexOf(this.query.toLowerCase()) > -1;
+      });
+    }
+  },
+  watch: {
+    maList: {
+      handler () {
+        localStorage.setItem(CLE_VALEURS_LISTE, JSON.stringify(this.maList));
+      },
+      deep: true
+    }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  .ws{
+    background-color: whitesmoke;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+  img{
+    max-width: 150px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,74 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-// import App from './App';
-// import router from './router';
+import App from './App';
+import router from './router';
 
 Vue.config.productionTip = false;
-
-const CLE_VALEURS_LISTE = 'ValeursListe';
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  data () {
-    let maList;
-    try {
-      maList = JSON.parse(localStorage.getItem(CLE_VALEURS_LISTE));
-
-      // Protection si null
-      if (!maList) {
-        throw new Error();
-      }
-    } catch (e) {
-      maList = [{
-        nom: 'Normand',
-        prenom: 'Normand',
-        couleur: '00FFFF'
-      }];
-    }
-
-    return {
-      on: true,
-      message: 'Quel robot es-tu ?',
-      message2: '',
-      nom: '',
-      prenom: '',
-      couleur: '',
-      query: '',
-      maList
-    };
-  },
-  methods: {
-    ajouterPersonneListe () {
-      this.maList.push({nom: this.nom, prenom: this.prenom, couleur: this.couleur});
-    },
-    retirerPersonneListe (personne) {
-      this.maList.splice(this.maList.indexOf(personne), 1);
-    }
-  },
-  filters: {
-    enAvatar (personne, index) {
-      index++;
-      return 'https://robohash.org/' + personne.prenom + personne.nom + '/';
-    }
-  },
-  computed: {
-    listeFiltree () {
-      return this.maList.filter(personne => {
-        return personne.nom.toLowerCase().indexOf(this.query.toLowerCase()) > -1 || personne.prenom.toLowerCase().indexOf(this.query.toLowerCase()) > -1;
-      });
-    }
-  },
-  watch: {
-    maList: {
-      handler () {
-        localStorage.setItem(CLE_VALEURS_LISTE, JSON.stringify(this.maList));
-      },
-      deep: true
-    }
-  }
-  // router,
-  // template: '<App/>',
-  // components: { App }
+  router,
+  template: '<App/>',
+  components: { App }
 });
