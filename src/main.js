d34cb5fd7f5b472105398212a9ec634c54ae5110
normandparatte <normand.paratte@gmail.com>
2017-07-03 13:06:58 +0200

    Exemple avec ajout de quelques lignes en vuejs: - Affichage d'un titre si variable =on - Affichage de plusieurs personnes (v-for) - Ajout d'une class avec v-bind:class - Ajout d'une couleur sur la personne avec v-bind:style - Affichage d'une image personnalisée selon l'index

--- src/main.js
+++ src/main.js
@@ -1,15 +1,37 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import App from './App';
-import router from './router';
+// import App from './App';
+// import router from './router';
 
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+  data () {
+    return {
+      on: true,
+      message: 'Hello world',
+      maList: [{
+        nom: 'Paratte',
+        prenom: 'Normand',
+        couleur: '00EEEE'
+      },
+      {
+        nom: 'Voyame',
+        prenom: 'Mael',
+        couleur: 'EE00EE'
+      }]
+    };
+  },
+  filters: {
+    enAvatar (personne, index) {
+      index++;
+      return 'https://api.adorable.io/avatars/face/eyes' + index + '/nose' + index + '/mouth' + index + '/' + personne.couleur;
+    }
+  }
+  // router,
+  // template: '<App/>',
+  // components: { App }
 });
